17ca7ee9ba247b519eebea9cb64bf04813786252
lst <luoshutu@qq.com>
2021-04-26 14:32:56 +0800

    日常提交

--- usbApplication/mainWindow.cpp
+++ usbApplication/mainWindow.cpp
@@ -4,27 +4,89 @@ mainWindow::mainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
-	bool ok = InitDevice();
-	std::map<UINT, std::string> USBDeviceMap;
-	GetUSBDeviceMap(&USBDeviceMap);
 
-	qDebug() << QString::fromStdString( USBDeviceMap[0] );
+	Init();
+}
 
-	if (ok)
+// 初始化
+void mainWindow::Init()
+{
+	// 数据初始化
+	data = (UCHAR *)malloc(1024 * sizeof(UCHAR));
+	memset(data, 12, 1024 * sizeof(UCHAR));		// 发射数据缓存
+	inData = (UCHAR *)malloc(1024 * sizeof(UCHAR));
+	memset(inData, 0, 1024 * sizeof(UCHAR));	// 接收数据缓存
+
+	// 初始化USB设备
+	InitUSBDeviceISOK = false;
+	InitUSBDeviceISOK = InitDevice();
+
+	if (InitUSBDeviceISOK)
 	{
-		PUCHAR data = (UCHAR *)malloc(1024 * sizeof(UCHAR));
-		memset(data, 12, 1024 * sizeof(UCHAR));		// 发射数据缓存
-		PUCHAR inData = (UCHAR *)malloc(1024 * sizeof(UCHAR));
-		memset(inData, 0, 1024 * sizeof(UCHAR));		// 发射数据缓存
-		
+		GetUSBDeviceMap(&USBDeviceMap);
+		for (UCHAR i = 0; i < USBDeviceMap.size(); i++)
+		{
+			qDebug() << QString("USBDeviceMap:%1").arg(i) << QString::fromStdString(USBDeviceMap[i]);
+			ui.comboBoxUSBDeviceSelect->addItem(QString::fromStdString(USBDeviceMap[i]));
+		}
 		SelectDevice(0);
 		SetBulkLoopData(1024, data, inData);
-		SetBulkLoopTestData(1024, 5, 2);
-		//StartBulkLoopTest();
-		StartBulkLoop();
-		for (int i = 0; i < 10; i++)
-		{
-			qDebug() << inData[i];
-		}
+	}
+
+	// 初始化计时器
+	m_timerUpdateDevice = new QTimer;
+	//m_timerUpdateDevice->start(20);
+	connect(m_timerUpdateDevice, SIGNAL(timeout()), this, SLOT(slot_updateComBoxDevice()));
+}
+
+// 更新设备
+void mainWindow::slot_updateComBoxDevice()
+{
+
+}
+
+// 选择设备
+void mainWindow::on_comboBoxUSBDeviceSelect_currentIndexChanged()
+{
+	if (InitUSBDeviceISOK)
+	{
+		int USBDeviceIndex = ui.comboBoxUSBDeviceSelect->currentIndex();
+		qDebug() << "Select USBDevice :" << USBDeviceIndex << " " 
+			<< QString::fromStdString(USBDeviceMap[USBDeviceIndex]) << endl;
+		
+		SelectDevice(USBDeviceIndex);
 	}
 }
+
+// 输入待传输的数据
+void mainWindow::on_textEditTXData_textChanged()
+{
+	QString transData = ui.textEditTXData->toPlainText();
+	
+	for (int i = 0; i < transData.size(); i++)
+	{
+		data[i] = transData.toStdString()[i];
+	}
+	SetBulkLoopData(1024, data, inData);
+	//SetBulkLoopTestData(1024, 5, 2);
+}
+
+
+// 开始Loop
+void mainWindow::on_pushButtonStart_clicked()
+{
+	if (InitUSBDeviceISOK)
+	{
+		StartBulkLoop();
+	}
+
+	QString inputData = "";
+
+	for (UCHAR i = 0; i < 32; i++)
+	{
+		inputData.append(QString("%1  ").arg(inData[i]));
+	}
+
+	ui.textBrowserRXData->append(inputData);
+}
+
--- usbApplication/mainWindow.h
+++ usbApplication/mainWindow.h
@@ -5,7 +5,9 @@
 #include "ui_mainWindow.h"
 #include <QDebug>
 #include <QMap>
-#include ".\Hpp\usbCommunication.h"
+#include <QTimer>
+#include <string>
+#include ".\Hpp\dataTransfer.h"
 
 class mainWindow : public QMainWindow
 {
@@ -13,6 +15,23 @@ class mainWindow : public QMainWindow
 
 public:
     mainWindow(QWidget *parent = Q_NULLPTR);
+	void Init();
+
+public slots:
+	void on_pushButtonStart_clicked();
+	void on_comboBoxUSBDeviceSelect_currentIndexChanged();
+	void on_textEditTXData_textChanged();
+
+	void slot_updateComBoxDevice();
+
+private:
+	bool InitUSBDeviceISOK;
+
+	PUCHAR data;		// 输出数据
+	PUCHAR inData;		// 输入数据
+
+	std::map<UINT, std::string> USBDeviceMap;		// USB设备Map
+	QTimer *m_timerUpdateDevice;					// 计时器，更新USB设备显示
 
 private:
     Ui::mainWindowClass ui;
